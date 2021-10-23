8346734a26c1a8b089b283170ef67f2221005325
SHC <shahar.haramati@gmail.com>
2021-10-23 18:32:21 +0300

    flip cards basic

--- src/App.js
+++ src/App.js
@@ -2,30 +2,40 @@ import './App.css';
 import Images from './Images';
 import {useState} from 'react';
 import {shuffle} from 'lodash';
+import BackSide from "./img/js-badge.svg";
 
 {/* <div style={{ background: `url(${background_img}) center no-repeat`, backgroundSize: 'cover' }}> */}
 //  style={{ width: '90%', height: '90%'}}
 
 function App() {
 
-  // const [cards, setCards] = useState(new Array());
   const [cards, setCards] = useState(shuffle([...Images, ...Images]));
+  const [activeCards, setActiveCards] = useState([]);
+
+  function flipCard(index){
+    // console.log("index: ", index);
+    // console.log("cards[index]: ", cards[index]);
+
+
+    setActiveCards([index]);
+  }
 
   return (
     <div style={{paddingBottom: '10px' }}>
       <header>
         TEST
       </header>
-
+      
+      {/* + (activeCards.indexOf(index) ? 'flipped' : '') */}
       <div className="board">
         {cards.map((card, index) => (
-          <div className="card-outer">
+          <div className={"card-outer " + (activeCards.indexOf(index) !== -1 ? 'flipped' : '')} onClick={ () => flipCard(index) }>
             <div className="card">
               <div className="front">
                 <img src={card} title="" />
               </div>
               <div className="back">
-
+                <img src={BackSide} title="" />
               </div>
             </div>
           </div>
